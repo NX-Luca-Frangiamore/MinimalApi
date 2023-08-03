58375f6b6f0bdae507783299fcd4f07264366b3d
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-03 11:23:31 +0200

    creazione class astratta per accesso dati

---
+++ AccessData.cs
@@ -0,0 +1,20 @@
+﻿using MinimalApi.Model;
+
+namespace MinimalApi
+{
+    public abstract class AccessData
+    {
+        readonly private Context context;
+        public AccessData(Context context) { 
+            this.context = context;
+        }
+        abstract public Task<IEnumerable<Utente>> getUtenti();
+        abstract public Task<IEnumerable<NTelefono>> getNumero(int idUtente);
+        abstract public Task<IEnumerable<NEmail>> getEmail(int idUtente);
+
+        abstract public void addUtenti(Utente utente);
+        abstract public void addNumero(int idUtente, NTelefono numero);
+        abstract public void addEmail(int idUtente, NEmail email);
+
+    }
+}
---
+++ Model/Utente.cs
@@ -0,0 +1,12 @@
+﻿namespace MinimalApi.Model
+{
+    public class Utente
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string cognome { get; set; }
+        public string indirizzo { get; set; }
+        public ICollection<NTelefono> nTelefoni { get; set;}
+        public ICollection<NEmail> nEmails { get; set; }
+    }
+}
