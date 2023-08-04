c9fefd4c9f5f6b0ff6b553924384897edc5deabc
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-04 10:15:01 +0200

    utilizzo gitingnore

--- Models/Utente.cs
+++ Models/Utente.cs
@@ -5,12 +5,12 @@
        
 
         public int id { get; set; }
-        public string name { get; set; }
+        public string nome { get; set; }
         public string cognome { get; set; }
         public string? indirizzo { get; set; }
         public override string ToString()
         {
-            return $"nome {name}, cognome {cognome}, indirizzo {indirizzo}";
+            return $"nome {nome}, cognome {cognome}, indirizzo {indirizzo}";
         }
     }
 }
