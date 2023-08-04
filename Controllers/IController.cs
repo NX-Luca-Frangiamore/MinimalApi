7aa84ced7f06b2b133f137a1d208a9b11471e364
NX-Luca-Frangiamore <luca.frangiamore@nexusat.it>
2023-08-04 10:21:31 +0200

    inserimento endpoint reset

--- Controllers/IController.cs
+++ Controllers/IController.cs
@@ -8,7 +8,13 @@ namespace MinimalApi.Controllers
     {
         protected AccessData dati;
         public IController(AccessData dati) { this.dati = dati; }
-      
-        
+
+        [Route("Reset")]
+        public void reset()
+        {
+            dati.resetDati();
+        }
+
+
     }
 }
